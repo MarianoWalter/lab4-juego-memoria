c400b498d766f8f7386c419c1629d7d68bdbe5de
MarianoWalter <marianowalter.14@gmail.com>
2019-08-30 20:07:13 -0300

    Botón de reiniciar y mensaje al perder

--- src/index2.js
+++ src/index2.js
@@ -5,6 +5,10 @@
 let UI = {
 	tabla: document.getElementById('tabla'),
 	celdas: [],
+	msgIntentos: document.getElementById('msg-intentos'),
+	actualizarIntentos: function() {
+		UI.msgIntentos.textContent = `Intentos restantes: ${Juego.maximoIntentos - Juego.intentos} de ${Juego.maximoIntentos}`;
+	},
 
 	// Elementos de la versión 2
 	v2: document.getElementsByClassName('v2'),
@@ -12,16 +16,30 @@ let UI = {
 	/** @type { HTMLButtonElement } */
 	btnReiniciar: document.getElementById('btn-reiniciar'),
 
+	setupInicial: function() {
+		// Para mostrar los elementos de la v2
+		document.querySelectorAll('.v2').forEach(x => x.removeAttribute('hidden'));
+
+		UI.btnReiniciar.onclick = () => {
+			let reiniciar = true;
+			if (!Juego.finIntentos()) {
+				confirm('¿Seguro que desea reiniciar el juego?');
+			}
+
+			if (reiniciar) {
+				Juego.reiniciar();
+			}
+		};
+	},
+
 	resetTabla: function() {
 		UI.tabla.innerHTML = '';
 		UI.celdas = [];
 	},
 
 	reset: function() {
-		// Para mostrar los elementos de la v2
-		document.querySelectorAll('.v2').forEach(x => x.removeAttribute('hidden'));
-
 		UI.resetTabla();
+		UI.actualizarIntentos();
 
 		// Crea las celdas en el HTML
 		for (let i = 0; i < Juego.dimensiones.alto; i++) {
@@ -67,7 +85,13 @@ let Juego = {
 	pausa: false,
 
 	maximoIntentos: 10,
-	intentos: 10,
+	intentos: 0,
+	finIntentos: function() {
+		return Juego.intentos >= Juego.maximoIntentos;
+	},
+
+	// Milisegundos que demora el juego en volver a ocultar 2 cartas seleccionadas que no son iguales
+	tiempoMuestra: 500,
 
 	/** @type { Carta[] } */
 	cartas: [],
@@ -103,10 +127,10 @@ let Juego = {
 	},
 
 	reiniciar: function() {
+		Juego.intentos = 0;
 		UI.reset();
 
-		this.intentos = 0;
-		this.setCartas();
+		Juego.setCartas();
 	},
 
 	/**
@@ -118,20 +142,42 @@ let Juego = {
 			return;
 		}
 
-		Juego.cartaSeleccionada.setSeleccionada(false);
+		// Juego.cartaSeleccionada.setSeleccionada(false);
 		Juego.cartaSeleccionada = null;
 
+		// Si las 2 cartas no son iguales
 		if (carta1.valor !== carta2.valor) {
-			// TODO sumar intento
-			return;
+			Juego.pausa = true;
+			Juego.intentos++;
+
+			carta2.setSeleccionada(true);
+
+			// Timeout para mostrar las cartas que se seleccionaron
+			return setTimeout(() => {
+				carta1.setSeleccionada(false);
+				carta2.setSeleccionada(false);
+				Juego.pausa = false;
+				UI.actualizarIntentos();
+
+				// Si llega al límite de intentos
+				if (Juego.finIntentos()) {
+					Juego.cartas.filter(carta => !carta.adivinada)
+						.forEach(carta => carta.celda.classList.add('no-adivinada'));
+
+					// Timeout para que actualice la interfaz antes de mostrar el mensaje
+					setTimeout(() => alert('Se llegó al límite de intentos, has perdido!'), 50);
+				}
+			}, Juego.tiempoMuestra);
 		}
 
+		// Si las 2 cartas SON iguales
 		for (let carta of [carta1, carta2]) {
 			carta.setSeleccionada(false);
 			carta.adivinada = true;
 			carta.celda.classList.add('adivinada');
-		}
 
+			// TODO verificar si se adivinaron todas
+		}
 	},
 
 };
@@ -172,8 +218,7 @@ function Carta(valor) {
 	/** @type {(c: Celda) => void} */
 	this.setCelda = function(celda) {
 		function clickEnCelda() {
-			console.info({ celda, carta }); // TODO borrar
-			if (Juego.pausa || carta.adivinada || Juego.cartaSeleccionada === carta) {
+			if (Juego.pausa || Juego.finIntentos() || carta.adivinada || Juego.cartaSeleccionada === carta) {
 				return;
 			}
 
@@ -189,5 +234,5 @@ function Carta(valor) {
 }
 
 
-// UI.reset();
+UI.setupInicial();
 Juego.reiniciar();
